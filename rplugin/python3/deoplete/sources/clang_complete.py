916a6ead31cf7f140b2a199ce60c75ec189bd6b1
kinkalow <kinkalow90@gmail.com>
2022-01-05 11:57:05 +0900

    Remove duplicate completion candidates

--- rplugin/python3/deoplete/sources/clang_complete.py
+++ rplugin/python3/deoplete/sources/clang_complete.py
@@ -10,7 +10,7 @@ class Source(Base):
         Base.__init__(self, vim)
 
         self.name = 'clang_complete'
-        self.mark = '[clang]'
+        self.mark = '[CL]'
         self.filetypes = ['c', 'cpp', 'objc', 'objcpp']
         self.is_bytepos = True
         self.input_pattern = '[^. \t0-9]\.\w*'
@@ -19,4 +19,15 @@ class Source(Base):
         return self.vim.call('ClangComplete', 1, 0)
 
     def gather_candidates(self, context):
-        return self.vim.call('ClangComplete', 0, '')
+        dup = self.vim.eval('g:__my_clang_complete_duplicate')
+        if dup:
+          return self.vim.call('ClangComplete', 0, '')
+        else:
+          results = self.vim.call('ClangComplete', 0, '')
+          items = []
+          for result in results[:]:
+            if result['abbr'] in items:
+              results.remove(result)
+            else:
+              items.append(result['abbr'])
+          return results
